87b763687ee19a20c5765fdaa0458fb867c03d20
Christopher Stevenson <ccstevenson@live.com>
2014-09-23 18:14:11 -0600

    Created initial migration.

--- backend/goals/goals/settings.py
+++ backend/goals/goals/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.public'
+    'apps.public',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
