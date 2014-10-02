f141dc26c02aead0d95ca8dd56abc21382130067
codingchick <heather@codingchick.com>
2014-10-01 19:49:49 -0600

    made a new journal page

---
+++ frontend/app/journal/journal.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.journal', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+//  $routeProvider.when('/acheivements', {
+//    templateUrl: 'acheivements/acheivements.html',
+//    controller: 'acheivementsCtrl'
+//  });
+}])
+
+.controller('journalCtrl', [function() {
+
+}]);
