3b1906e8d1ccb4746711093cb6f99aec3e81a2e7
Michelle Floyd <michellelfloyd@yahoo.com>
2014-10-08 20:03:51 -0600

    add goal working, childgoal timeframe migration

---
+++ backend/goals/apps/public/migrations/0002_auto__add_field_childgoal_timeFrame.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ChildGoal.timeFrame'
+        db.add_column(u'public_childgoal', 'timeFrame',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['public.TimeFrame']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ChildGoal.timeFrame'
+        db.delete_column(u'public_childgoal', 'timeFrame_id')
+
+
+    models = {
+        u'public.childgoal': {
+            'Meta': {'object_name': 'ChildGoal'},
+            'goal': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'to': u"orm['public.Goal']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'step': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'timeFrame': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['public.TimeFrame']"})
+        },
+        u'public.goal': {
+            'Meta': {'object_name': 'Goal'},
+            'description': ('django.db.models.fields.TextField', [], {'default': "'No description has been entered yet'"}),
+            'dueDate': ('django.db.models.fields.DateField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'timeFrame': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['public.TimeFrame']"})
+        },
+        u'public.journal': {
+            'Meta': {'object_name': 'Journal'},
+            'entry': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'public.timeframe': {
+            'Meta': {'object_name': 'TimeFrame'},
+            'days': ('django.db.models.fields.IntegerField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['public']
