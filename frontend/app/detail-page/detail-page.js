95c3bf4d4264ab907f4fca99b493af089aeb1d98
Matt Jensen <mjensen6889@gmail.com>
2014-10-08 20:37:13 -0600

    Styling changes on detail page, also fixed edit button on active steps but not complete

--- frontend/app/detail-page/detail-page.js
+++ frontend/app/detail-page/detail-page.js
@@ -2,18 +2,29 @@
 
 angular.module('myApp.detail-page', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/detail-page/:goalId', {
-    templateUrl: 'detail-page/detail-page.html',
-    controller: 'DetailPageCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/detail-page/:goalId', {
+            templateUrl: 'detail-page/detail-page.html',
+            controller: 'DetailPageCtrl'
+        });
+    }])
 
-.controller('DetailPageCtrl', ["Restangular", "$scope", "$routeParams", function(Restangular, $scope, $routeParams) {
+    .controller('DetailPageCtrl', ["Restangular", "$scope", "$routeParams", function (Restangular, $scope, $routeParams) {
         var goalId = $routeParams.goalId;
-         Restangular.one("goals", goalId).get().then(function(goal){
+        Restangular.one("goals", goalId).get().then(function (goal) {
             $scope.goal = goal
         });
 
-        $scope.editButtons = "../app/partials/edit-buttons.html";
-}]);
+
+        $scope.inactiveButtons = "../app/partials/inactive-buttons.html";
+        $scope.activeButtons = "../app/partials/active-buttons.html";
+
+        $scope.statusChange = function (childGoal, status) {
+            childGoal.status = status;
+
+            Restangular.one("child-goals", childGoal.id).customPUT(childGoal).then(function (chGoal) {
+                childGoal = chGoal;
+            });
+        }
+
+    }]);
