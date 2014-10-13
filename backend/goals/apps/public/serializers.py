dbf608c6a7b84a34164a74f968718fc55e034bbb
Michelle Floyd <michellelfloyd@yahoo.com>
2014-10-13 17:44:58 -0600

    Achievements page ready for critique

--- backend/goals/apps/public/serializers.py
+++ backend/goals/apps/public/serializers.py
@@ -1,9 +1,13 @@
 from models import *
 from rest_framework import serializers
+from datetime import date
+from datetime import timedelta
 
 
 class GoalSerializer(serializers.ModelSerializer):
     child_goals = serializers.SerializerMethodField('get_child_goals')
+    goal_progress = serializers.SerializerMethodField('get_goal_progress')
+    goal_time = serializers.SerializerMethodField('get_goal_time')
 
     class Meta:
         model = Goal
@@ -12,6 +16,20 @@ class GoalSerializer(serializers.ModelSerializer):
     def get_child_goals(self, obj):
         children = obj.children.all()
         return ChildGoalSerializer(children, many=True).data
+
+    def get_goal_progress(self, obj):
+        children = obj.children.all()
+        completed = children.filter(status=True).count()
+        total = children.count()
+        return round((float(completed)/total) * 100)
+
+    def get_goal_time(self, obj):
+        today = date.today()
+        deadline = obj.dueDate
+        dayspan = obj.timeFrame.days
+        start = deadline - timedelta(days=dayspan)
+        dayspassed = today - start
+        return round((float(dayspassed.days) / dayspan) * 100)
 
 
 class TimeFrameSerializer(serializers.ModelSerializer):
