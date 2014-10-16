1adbf14b0cd6c3d2e959922f799b342aa21bed93
Matt Jensen <mjensen6889@gmail.com>
2014-10-15 18:19:34 -0600

    started addStep funcitonality

--- frontend/app/detail-page/detail-page.js
+++ frontend/app/detail-page/detail-page.js
@@ -15,10 +15,29 @@ angular.module('myApp.detail-page', ['ngRoute'])
             $scope.goal = goal
         });
 
-
         $scope.inactiveButtons = "../app/partials/inactive-buttons.html";
         $scope.activeButtons = "../app/partials/active-buttons.html";
         $scope.completeButtons = "../app/partials/complete-buttons.html";
+
+//        SECTION I'M WORKING ON TO ADD STEP
+        $scope.stepAdd = function (keyEvent, goal, step) {
+            if (keyEvent.which === 13) {
+
+                var childGoal = {
+                    "goal": goal.id,
+                    "step": step,
+                    "timeFrame": 1
+                };
+
+                Restangular.one("child-goals/").customPOST(childGoal).then(function (step) {
+                    goal.step = step;
+                });
+                goal.addStepEdit = false;
+            }
+        };
+
+//        END OF SECTION I'M WORKING ON
+
 
         $scope.statusChange = function (childGoal, status) {
             childGoal.status = status;
@@ -26,7 +45,7 @@ angular.module('myApp.detail-page', ['ngRoute'])
             Restangular.one("child-goals", childGoal.id).customPUT(childGoal).then(function (chGoal) {
                 childGoal = chGoal;
             });
-        }
+        };
 
         $scope.completeChange = function (childGoal, complete) {
             childGoal.complete = complete;
@@ -48,33 +67,33 @@ angular.module('myApp.detail-page', ['ngRoute'])
             }
         };
 
-        $scope.activeGoalsEmpty = function(goal) {
+        $scope.activeGoalsEmpty = function (goal) {
             if ($scope.hasOwnProperty('goal')) {
                 return $scope.goal.child_goals.filter(returnStatus).length;
             }
         };
 
-        var returnStatus = function(elem) {
+        var returnStatus = function (elem) {
             return elem.status && !elem.complete
         }
 
-        $scope.inactiveGoalsEmpty = function(goal) {
+        $scope.inactiveGoalsEmpty = function (goal) {
             if ($scope.hasOwnProperty('goal')) {
                 return $scope.goal.child_goals.filter(returnStatus2).length;
             }
         };
 
-        var returnStatus2 = function(elem) {
+        var returnStatus2 = function (elem) {
             return elem.status
         }
 
-        $scope.completeGoalsEmpty = function(goal) {
+        $scope.completeGoalsEmpty = function (goal) {
             if ($scope.hasOwnProperty('goal')) {
                 return $scope.goal.child_goals.filter(returnStatus3).length;
             }
         };
 
-        var returnStatus3 = function(elem) {
+        var returnStatus3 = function (elem) {
             return elem.complete
         }
 
